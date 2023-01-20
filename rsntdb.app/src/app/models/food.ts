7d63886c57d90e20497a0022533dfff025f9bb13
skynet <sowbeari@ucsc.edu>
2023-01-19 20:48:42 -0800

    food service

---
+++ rsntdb.app/src/app/models/food.ts
@@ -0,0 +1,10 @@
+export class Food {
+    id: number;
+    name: string;
+
+    constructor(id:number, name:string)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
--- rsntdb.app/src/app/services/table/food.service.ts
+++ rsntdb.app/src/app/services/table/food.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { /*BehaviorSubject,*/ Observable/*, of */} from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+import { Food } from 'src/app/models/food';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  private endpoint = "api/food";
+  private endpoint: string = "food";
 
-  
+  constructor(private http: HttpClient) { }
+
+
+  public getAllFood(): Observable<Food[]> {
+    return this.http.get<Food[]>(environment.baseUrl + this.endpoint);
+  }
+
+  public getAFood(id: number): Observable<Food> {
+    return this.http.get<Food>(environment.baseUrl + this.endpoint + id);
+  }
 }
